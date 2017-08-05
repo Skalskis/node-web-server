e21356208e4f66ad855d34aa07a2acb436fa0018
Miša Skalskis <misa@misa-3.local>
2017-08-05 12:32:06 +0300

    Added Projects Page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 
 var app = express();
 
@@ -43,6 +43,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: "About Page",
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: "Projects Page",
   });
 });
 
